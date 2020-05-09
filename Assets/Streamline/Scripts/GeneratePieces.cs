f8cf87eb1459196ec00664312593d0cfed9460fb
Sanyo <sanyo.gorbunov@gmail.com>
2020-05-09 10:20:55 +0200

    Added glass scene

---
+++ Assets/Streamline/Scripts/GeneratePieces.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class GeneratePieces : MonoBehaviour
+{
+    public int pieceCount;
+    public Transform point;
+    public GameObject prefab;
+    public float radius;
+    public PlacementType placementType;
+
+    public void Start()
+    {
+        switch (placementType)
+        {
+            case PlacementType.Surface:
+                break;
+            case PlacementType.Flying:
+                break;
+            case PlacementType.FlyingAroundPlayer:
+                InstantiateCircle();
+                break;
+        }
+    }
+
+    void InstantiateCircle()
+    {
+        float angle = 360f / (float)pieceCount;
+        for (int i = 0; i < pieceCount; i++)
+        {
+            Quaternion rotation = Quaternion.AngleAxis(i * angle, Vector3.up);
+            Vector3 direction = rotation * Vector3.forward;
+
+            Vector3 position = point.position + (direction * radius);
+            Instantiate(prefab, position, rotation);
+        }
+    }
+}
+
+public enum PlacementType
+{
+    Surface,
+    Flying,
+    FlyingAroundPlayer
+}
